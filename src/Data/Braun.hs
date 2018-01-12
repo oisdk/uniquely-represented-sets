fda04a2fb2c4561c8fa719b76144825236af20eb
Donnacha Oisín Kidney <mail@doisinkidney.com>
2018-01-12 17:06:25 +0000

    very churchy now

--- src/Data/Braun.hs
+++ src/Data/Braun.hs
@@ -37,12 +37,15 @@ toList (Node x ys zs) = x:go [ys,zs] [] []
 --
 -- prop> fromList' xs == preFromList (xs :: [Int])
 fromList' :: [a] -> Tree a
-fromList' = head . rows where
-  make k xs ts = zipWith3 Node xs ts1 ts2 where
-    (ts1,ts2) = splitAt k (ts ++ repeat Leaf)
-  rows xs = snd (foldr f b xs 1 1) where
-    f e a !k 1 = let (ys,zs) = a (k*2) k in ([], make k (e:ys) zs)
-    f e a !k m = let (ys,zs) = a k (m-1) in (e:ys,zs)
-    b _ _ = ([],[Leaf])
-
--- z3n k xs ts =
+fromList' = head . snd . rows
+  where
+    rows xs = foldr f (\_ _ -> (\_ _ -> [], [Leaf])) xs 1 1
+    f e a !k m =
+        case m of
+            1 -> (\_ _ -> [], g e ys k zs (drop k zs)) where (ys,zs) = a (k * 2) k
+            _ -> (g e ys m, zs) where (ys,zs) = a k (m - 1)
+    g _ _ 0 _ _ = []
+    g x xs !_ (y:ys) (z:zs) = Node x y z : xs ys zs
+    g x xs !_ [] (z:zs) = Node x Leaf z : xs [] zs
+    g x xs !_ (y:ys) [] = Node x y Leaf : xs ys []
+    g x xs !_ [] [] = Node x Leaf Leaf : xs [] []
