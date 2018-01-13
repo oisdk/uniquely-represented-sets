5605b6bf102bd36112b1c000c53a6682469eb986
Donnacha Oisín Kidney <mail@doisinkidney.com>
2018-01-13 02:36:32 +0000

    types

--- bench/bench.hs
+++ bench/bench.hs
@@ -3,6 +3,15 @@ module Main (main) where
 import Criterion.Main
 import Data.Braun
 
-main = defaultMain
-   [ bench "2" $ nf fromList   [(1 :: Int)..10000]
-   , bench "1" $ nf preFromList [(1 :: Int)..10000]]
+toListSize :: Int -> Benchmark
+toListSize n =
+    env (pure (fromList [1 .. n])) $
+    \xs ->
+         bgroup
+             (show n)
+             [bench "old" $ nf toList' xs, bench "new" $ nf toList xs]
+
+main =
+    defaultMain
+        [ toListSize (10 ^ n)
+        | n <- [3 .. 8] ]
--- src/Data/Braun.hs
+++ src/Data/Braun.hs
@@ -1,10 +1,12 @@
-{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE BangPatterns        #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE RankNTypes          #-}
 
 module Data.Braun where
 
 -- import qualified Data.Tree as Tree
-import Data.Tree (Tree(..))
-import GHC.Base (build)
+import           Data.Tree (Tree (..))
+import           GHC.Base  (build)
 
 -- preFromList :: [a] -> Tree a
 -- preFromList [] = Leaf
@@ -26,12 +28,15 @@ import GHC.Base (build)
 --   in (Node x odx evx, Node y ody evy)
 
 toList :: Tree a -> [a]
-toList t = build (\c n ->
-  let b _ [] = n
-      b r s = revf go r (revf go s b) [] []
-      go Leaf ps l r = ps l r
+toList t = build (\(c :: a -> b -> b) (n :: b) ->
+  let b :: [Tree a] -> [Tree a] -> b
+      b [] _ = n
+      b _ [] = n
+      b r s  = foldr revf id r (foldr revf id s b) [] []
+      go :: Tree a -> ([Tree a] -> [Tree a] -> b) -> [Tree a] -> [Tree a] -> b
+      go Leaf ps l r           = ps l r
       go (Node p qs rs) ss l r = p `c` ss (qs:l) (rs:r)
-      revf f = foldr (flip (.) . f) id
+      revf x y z  = y (go x z)
   in go t b [] [])
 
 -- |
@@ -40,7 +45,7 @@ toList t = build (\c n ->
 fromList :: [a] -> Tree a
 fromList xs = foldr f (\_ _ p -> p b [Leaf]) xs 1 1 (const head)
   where
-    f e a !k 1 p = a (k*2) k (\ys zs -> p b (g e ys zs (drop k zs)))
+    f e a !k 1 p  = a (k*2) k (\ys zs -> p b (g e ys zs (drop k zs)))
     f e a !k !m p = a k (m-1) (p . g e)
 
     b _ _ = []
@@ -50,3 +55,27 @@ fromList xs = foldr f (\_ _ p -> p b [Leaf]) xs 1 1 (const head)
     g x a (y:ys) []     = Node x y    Leaf : a ys []
     g x a [] []         = Node x Leaf Leaf : a [] []
     {-# NOINLINE g #-}
+
+toList' Leaf = []
+toList' t = tol [t]
+  where tol [] = []
+        tol ts = xs ++ tol (ts1 ++ ts2)
+          where xs = map root ts
+                (ts1,ts2) = children ts
+
+                children [] = ([],[])
+                children (Node _ Leaf _ : _) = ([],[])
+                children (Node _ a Leaf : ts) = (a : leftChildren ts, [])
+                children (Node _ a b : ts) = (a : ts1, b : ts2)
+                  where (ts1, ts2) = children ts
+                children _ = error "BraunSeq.toList: bug!"
+
+                leftChildren [] = []
+                leftChildren (Node _ Leaf _ : _) = []
+                leftChildren (Node _ a _ : ts) = a : leftChildren ts
+                leftChildren _ = error "BraunSeq.toList: bug!"
+
+                root (Node x _ _) = x
+                root _            = error "BraunSeq.toList: bug!"
+
+                (Node _ a _) = a
